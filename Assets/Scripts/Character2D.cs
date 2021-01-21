edae3c53e6868daf28580a5e575da8d058585f66
armezaleyva <armezaleyva@gmail.com>
2021-01-21 11:07:17 -0700

    Clase 2021-01-21

--- Assets/Scripts/Character2D.cs
+++ Assets/Scripts/Character2D.cs
@@ -7,25 +7,47 @@ public class Character2D : MonoBehaviour
     Animator anim;
 
     SpriteRenderer spr;
+
+    Rigidbody2D rb2D;
     
-    [SerializeField, Range(0.1f, 20f)]
+    [SerializeField, Range(0.1f, 15f)]
     float moveSpeed = 2f;
+
+    [SerializeField, Range(0.1f, 10f)]
+    float jumpForce;
+
+    [SerializeField]
+    Color rayColor = Color.magenta;
+    [SerializeField, Range(0.1f, 15f)]
+    float rayDistance = 5f;
+    [SerializeField]
+    LayerMask groundLayer;
 
     void Awake() 
     {
         anim = GetComponent<Animator>();
         spr = GetComponent<SpriteRenderer>();
+        rb2D = GetComponent<Rigidbody2D>();
     }
     
     void Update()
     {
         transform.Translate(Vector2.right * Axis.x * moveSpeed * Time.deltaTime);
-        spr.flipX = Flip;
     }
 
     void LateUpdate()
     {
+        spr.flipX = Flip;
         anim.SetFloat("moveX", Mathf.Abs(Axis.x));
+    }
+
+    // Para cosas de física
+    void FixedUpdate() 
+    {
+        if (jumpButton && IsGrounding)
+        {
+            rb2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
     }
 
     Vector2 Axis
@@ -38,4 +60,13 @@ public class Character2D : MonoBehaviour
         get => Axis.x > 0 ? false : Axis.x < 0f ? true : spr.flipX;
     }
 
+    bool jumpButton => Input.GetButtonDown("Jump");
+
+    bool IsGrounding => Physics2D.Raycast(transform.position, Vector2.down, rayDistance, groundLayer);
+
+    void OnDrawGizmosSelected() 
+    {
+        Gizmos.color = rayColor;
+        Gizmos.DrawRay(transform.position, Vector2.down * rayDistance);
+    }
 }
